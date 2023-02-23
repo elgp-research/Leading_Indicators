d2b2001f4d3e2d1c5dbae14f8907b09d7b26474c
Haseeb Bajwa <haseeb.sher.bajwa@gmail.com>
2023-02-23 11:24:28 -0500

    Removing old data analysis file

--- Data_Analysis.r
+++
@@ -1,9 +0,0 @@
-## Setting up code for Github repo
-
-library(tidyverse)
-data("iris")
-
-obs <- 100
-x <- rnorm(obs, mean = 1, sd = 0.5)
-
-ggplot(iris, aes(iris$Sepal.Length, iris$Sepal.Width)) + geom_point()
